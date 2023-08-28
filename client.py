6a6d400c2ce86126774204e50491d5277888028c
ILoveNetworking <v1p33r4l1fe@protonmail.com>
2023-08-28 02:09:33 -0400

    Added RSA encryption to the messages

--- client.py
+++ client.py
@@ -2,6 +2,9 @@
 
 import os
 import sys
+import rsa
+import pickle
+from base64 import b64decode, b64encode
 import socket
 import threading
 from random import randint
@@ -11,34 +14,68 @@ RENDEZVOUS_DATA = ("127.0.0.1", 65500) # Connect to localhost for now
 # Port on which peers will be connected to each other
 LISTENER_PEER_PORT = randint(50000, 65535)
 
-def connect_to_rendezvous() -> tuple:
+# Creating public private keys
+print("[+] Generating keypair for this session...")
+pub, priv = rsa.newkeys(2048)
+print("[+] Done!")
+
+# Assuming we are know the server public key
+server_pub_key = ""
+
+if os.path.exists("./certs/public.key"):
+    with open("./certs/public.key", "r") as spkf:
+        server_pub_key = pickle.loads(b64decode(spkf.read()))
+
+if server_pub_key == "":
+    print("[!] Failed to load server public key!")
+    print("[!] Please validate path")
+    sys.exit(-1)
+
+def connect_to_rendezvous() -> list:
     """
     This function connects to the rendezvous server to get the other peer connection data\n
     Arguments : None\n
-    Returns : tuple(str(remote_peer_ip), int(remote_peer_port))
+    Returns : list(tuple(str(remote_peer_ip), int(remote_peer_port)), PublicKey(remote_peer_public))
     """
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(("0.0.0.0", LISTENER_PEER_PORT))
 
-    control_packet = b"check"
+    based_pub_key = b64encode(pickle.dumps(pub)).decode()
+    control_packet = ("KEY " + based_pub_key).encode()
     control_packet_len = len(control_packet)
-    sock.sendto(control_packet, RENDEZVOUS_DATA)
+
+    # Workaround RSA encryption length, max chunck length 245
+    result = []
+    for n in range(0, control_packet_len, 245):
+        part = control_packet[n:n+245]
+        result.append( rsa.encrypt(part, server_pub_key) )
+    enc_control_packet = b''.join(result)
+
+    sock.sendto(enc_control_packet, RENDEZVOUS_DATA)
 
     while True:
-        response = sock.recv(256)
-        response = int(response.decode())
-        if response == control_packet_len:
+        response = sock.recv(1024)
+        dec_resp = rsa.decrypt(response, priv).decode()
+        pack_len = int(dec_resp)
+        if pack_len == control_packet_len:
             print(f"[+] Got response from server. Waiting another peer...")
             break
     
-    peer_data = sock.recv(256)
-    peer_data = peer_data.decode()
+    peer_data = sock.recv(2048)
+    result = []
+    for n in range(0,len(peer_data),256):
+        part = peer_data[n:n+256]
+        result.append( rsa.decrypt(part, priv).decode("ascii") )
+
+    peer_data = ''.join(result)
+
     print(f"[+] Recieved peer: {peer_data}")
-    ip,port = peer_data.split(":")
+    ip,port,remote_peer_pub = peer_data.split(":")
+    remote_peer_pub = pickle.loads(b64decode(remote_peer_pub))
     port = int(port)
     sock.close()
 
-    return (ip, port)
+    return [(ip, port), remote_peer_pub]
 
 def start_listener() -> None:
     """
@@ -52,9 +89,10 @@ def start_listener() -> None:
     # recieving messages here
     while True:
         msg, address = sock.recvfrom(1024)
+        decrypted = rsa.decrypt(msg, priv).decode()
 
-        if "FILE" in msg.decode().strip():
-            _, length, name = msg.decode().strip().split()
+        if "FILE" in decrypted.strip():
+            _, length, name = decrypted.strip().split()
             print(f"[+] Requested a file upload: {name} with length: {length}b")
             file_data = sock.recv(int(length) + 128)
             with open(f"./{name}", "wb") as f:
@@ -62,12 +100,12 @@ def start_listener() -> None:
             print(f"[+] File written: ./{name}\n> ")
             continue
 
-        print(f"\r[{address[0]}:{address[1]}]: {msg.decode()}\n> ", end="")
+        print(f"\r[{address[0]}:{address[1]}]: {decrypted}\n> ", end="")
 
-def send_msgs(con_data : tuple) -> None:
+def send_msgs(con_data : tuple, peer_pub : rsa.PublicKey) -> None:
     """
     This function sends messages to the remote peer\n
-    Arguments : tuple(str(remote_ip), int(remote_port))\n
+    Arguments : tuple(str(remote_ip), int(remote_port)), rsa.PublicKey(peer_pub)\n
     Returns : None
     """
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -90,6 +128,7 @@ def send_msgs(con_data : tuple) -> None:
             print("[+] \quit - exit the program")
             continue
         elif len(message.strip().split()) == 2:
+            # TODO: Implement encryption when sending file
             if message.strip().split()[0] == "\send_file":
                 _, file_path = message.strip().split()
                 if os.path.exists(file_path):
@@ -106,14 +145,16 @@ def send_msgs(con_data : tuple) -> None:
                     print(f"[+] Invalid path: {file_path}")
                     continue
 
-        sock.sendto(message.encode(), con_data)
+        encrypted = rsa.encrypt(message.encode(), peer_pub)
+        # assuming message is no longer than 245
+        sock.sendto(encrypted, con_data)
 
 if __name__ == "__main__":
     # connecting to rendezvous server to check in on server
-    peer_data = connect_to_rendezvous()
+    peer_data, peer_pub_key = connect_to_rendezvous()
     # start listener loop
     l_thread = threading.Thread(target=start_listener)
     l_thread.daemon = True
     l_thread.start()
     # Send messages to the remote peer
-    send_msgs(peer_data)
+    send_msgs(peer_data, peer_pub_key)
--- server.py
+++ server.py
@@ -1,10 +1,39 @@
 # UDP peer-to-peer chat rendezvous server
 
+import os
 import sys
+import rsa
+import pickle
+from base64 import b64decode, b64encode
 import socket
 import threading
 
 BIND_ADRESS = ("0.0.0.0", 65500)
+
+certfile = "./certs/public.key"
+cert_folder = "./certs"
+
+def check_cert_path() -> None:
+    """
+    This function checks if public and private keys exists\n
+    Arguments : None\n 
+    Returns : None
+    """
+    if not os.path.exists(certfile):
+        print("[!] No server certificate found!")
+        print("[+] Generating server private and public keys")
+        pub, priv = rsa.newkeys(2048, exponent=65535)
+        if not os.path.exists(cert_folder):
+            os.mkdir(cert_folder)
+        else:
+            return
+        with open(os.path.join(cert_folder, "public.key"), "w") as kf:
+            kf.write(b64encode(pickle.dumps(pub)).decode())
+        with open(os.path.join(cert_folder, "private.key"), "w") as cf:
+            cf.write(b64encode(pickle.dumps(priv)).decode())
+        print(f"[+] Keys generatet and written to: {cert_folder}/")
+        print("[+] Use some safe way to transfer public key to the client")
+
 
 def start_server() -> None:
     """
@@ -12,37 +41,66 @@ def start_server() -> None:
     Arguments : None\n
     Returns : None
     """
+    priv_key = ""
+    with open(os.path.join(cert_folder, "private.key"), "r") as cf:
+        priv_key = pickle.loads(b64decode(cf.read()))
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(BIND_ADRESS)
     print(f"[+] Starting a rendezvous server at: {BIND_ADRESS}")
 
+    # creating SSL socket
     # listening for incoming data
     clients = []
     while True:
 
-        data, address = sock.recvfrom(256)
-        data = data.decode()
+        data, address = sock.recvfrom(2048)
+        # data = data.decode()
 
-        if data.strip() == "check":
+        result = []
+        for n in range(0,len(data),256):
+            part = data[n:n+256]
+            result.append( rsa.decrypt(part, priv_key).decode() )
+
+        decrypted = ''.join(result)
+        print(decrypted)
+
+        if "KEY" in decrypted.strip(): # here client send it's public key after check
             print(f"[+] Connection from peer: {address}")
-            packet = str(len(data.strip())).encode()
-            sock.sendto(packet, address)
-            clients.append(address)
+            client_pub_key = pickle.loads(b64decode(decrypted.strip().split()[-1]))
+            packet = str(len(decrypted.strip())).encode()
+            enc_packet = rsa.encrypt(packet, client_pub_key)
+            sock.sendto(enc_packet, address)
+            clients.append([address, client_pub_key])
         
         if len(clients) == 2:
             c1 = clients[0]
             c2 = clients[1]
-            print(f"Exchanging: {c1} <-> {c2}")
-            packet_for_c2 = f"{c1[0]}:{c1[1]}".encode()
-            sock.sendto(packet_for_c2, c2)
-            packet_for_c1 = f"{c2[0]}:{c2[1]}".encode()
-            sock.sendto(packet_for_c1, c1)
+            print(f"Exchanging: {c1[0]} <-> {c2[0]}")
+            # exchanging the address:port:public_key between clients
+            packet_for_c2 = f"{c1[0][0]}:{c1[0][1]}:{b64encode(pickle.dumps(c1[1])).decode()}".encode()
+
+            result = []
+            for n in range(0, len(packet_for_c2), 245):
+                part = packet_for_c2[n:n+245]
+                result.append( rsa.encrypt(part, c2[1]) )
+            enc_packet_for_c2 = b''.join(result)
+            sock.sendto(enc_packet_for_c2, c2[0])
+            
+            packet_for_c1 = f"{c2[0][0]}:{c2[0][1]}:{b64encode(pickle.dumps(c2[1])).decode()}".encode()
+            result = []
+            for n in range(0, len(packet_for_c1), 245):
+                part = packet_for_c1[n:n+245]
+                result.append( rsa.encrypt(part, c1[1]) )
+            enc_packet_for_c1 = b''.join(result)
+            sock.sendto(enc_packet_for_c1, c1[0])
 
             clients = []
 
 
 if __name__ == "__main__":
     try:
+        check_cert_path()
         start_server()
     except KeyboardInterrupt:
         print("[+] KeyboardInterrupt detected. Exiting the program...")
